4b0def675491e139ccd65c54a04856587d878e8c
Jusitn Avioli <justin.avioli@gmail.com>
2019-03-03 23:32:34 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,12 @@
+//Chocolate bar array
+var chocolateBars = [
+  "snickers",
+  "hundred grand",
+  "kitkat",
+  "skittles"
+]
+
 //add an element to the beginning of an array
-function addElementToBeginningOfArray(array1, a){
-  array1.unshift(a)
+function addElementToBeginningOfArray(array, element){
+  array.unshift(element)
 }
